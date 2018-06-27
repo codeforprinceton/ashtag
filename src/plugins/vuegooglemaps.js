2583cb2edf005ca0ef38c071f14e2cb142e4b4f9
Shaun Ellis <shaun@sdellis.com>
2018-06-26 21:19:35 -0400

    WIP - removes firebase config from plugin and imports a config file that is gitignored

--- src/plugins/vuegooglemaps.js
+++ src/plugins/vuegooglemaps.js
@@ -1,9 +1,10 @@
 import * as VueGoogleMaps from 'vue2-google-maps'
+import {gmConfig} from '../helpers/googleMapsConfig'
 
 export default ({ Vue }) => {
   Vue.use(VueGoogleMaps, {
     load: {
-      key: 'AIzaSyC6Mxo0j7utiBOS9eW5qCvXQgPXHgJNIYI',
+      key: gmConfig.apiKey,
       libraries: 'places' // necessary for places input
     }
   })
